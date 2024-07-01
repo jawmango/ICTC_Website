e6265da89c24a3456966ac8d65528c1a4f89d4ff
jawmango <jjobiedo@gbox.adnu.edu.ph>
2024-07-01 16:19:19 +0800

    Update about.dart

--- lib/pages/desktop/about.dart
+++ lib/pages/desktop/about.dart
@@ -10,6 +10,7 @@ import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:ICTC_Website/widgets/carousel/carousel.dart';
 import 'package:flutter/material.dart';
+import 'package:ICTC_Website/widgets/drawerDesktop.dart';
 
 class AboutPage extends StatelessWidget {
   const AboutPage({super.key});
@@ -17,6 +18,7 @@ class AboutPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawerdesktop(),
       appBar: AppBarDesktop(),
       body: LayoutBuilder(
         builder: (context, constraints) {
